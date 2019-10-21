f4c74f7417a9aec9b7a3169ee8f4e608fb52e9e9
Jordan Fokoua <jfokoua@gmail.com>
2019-10-21 23:33:18 +0200

    :mute: Remove logs

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,6 @@
 exports.getAll = function(req, res) {
   try {
     const producTable = require("../database/products.json");
-    console.log("Log: exports.getAll -> producTable", producTable);
     if (producTable)
       return res
         .status(200)
@@ -16,9 +15,8 @@ exports.getAll = function(req, res) {
 exports.getAvailable = function(req, res) {
   try {
     const producTable = require("../database/products.json");
-    console.log("Log: exports.getAll -> producTable", producTable);
     if (producTable) {
-      let availableProducts = producTable.filter(
+      let availableProducts =  producTable.filter(
         product => product.available === true
       );
       return res
@@ -26,7 +24,7 @@ exports.getAvailable = function(req, res) {
         .json({ data: availableProducts, count: producTable.length });
     }
   } catch (error) {
-    console.log("Error::productController::getAll", error);
+    console.log("Error::productController::getAvailable", error);
     return res.status(500).json({ error: `Internal Server Error: ${error}` });
   }
 };
