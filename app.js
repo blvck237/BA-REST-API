7a306546e100e5d2341fc44f8021e759265db0fe
Jordan Fokoua <31737108+blvck237@users.noreply.github.com>
2019-10-21 15:52:08 +0200

    Resolev "2-generate-files" (#5)
    * New Eslint config
    
    * New scripts
    
    * Configure prettier and eslint
    
    * Generated files

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
