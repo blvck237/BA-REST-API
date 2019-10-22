9ec37466ca8285e3ef86338b2bfb95d3c96726d8
Jordan Fokoua <jfokoua@gmail.com>
2019-10-22 12:15:41 +0200

    :sparkles: Handle general errors

--- app.js
+++ app.js
@@ -20,10 +20,12 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
+  
+  console.log("Log: err", err);
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  return res.status(err.status || 500).json({
+    error: err.status === 500 ? `Internal Server Error: ${err}` : `${err}`
+  });
 });
 
 module.exports = app;
