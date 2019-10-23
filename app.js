ce63e75c7712585e332e1e5c9d3825926da3bbef
Jordan Fokoua <jfokoua@gmail.com>
2019-10-23 17:15:34 +0200

    :sparkles: :bug: Socket IO Setup. Emit productlist after 1 min. Fix Cors Issues

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const express = require('express');
+// const express = require('express');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, { origins: 'http://localhost:*' });
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -6,16 +9,20 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const products = require('./routes/products');
 
-const app = express();
+
+var interval;
+
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', products);
-app.use(cors());
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -27,6 +34,26 @@ app.use(function(err, req, res, next) {
   // render the error page
   return res.status(err.status || 500).json({
     error: err.status === 500 ? `Internal Server Error: ${err}` : `${err}`,
+  });
+});
+
+const emitProductList = async socket => {
+  try {
+    const producTable = require('./database/products.json');
+    socket.emit('refreshProducts', producTable); // Emitting a new message. It will be consumed by the client
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+
+io.on('connection', socket => {
+  console.log('New client connected');
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => emitProductList(socket), 60000);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
   });
 });
 
